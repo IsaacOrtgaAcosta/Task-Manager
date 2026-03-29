a40eca3135f1b7dbe0f5825c3011f36a69ded049
IsaacOrtgaAcosta <iortga.acosta@gmail.com>
2026-03-29 16:40:00 +0200

    feature: logup front-end is done

--- frontend/src/app/api/auth.api.js
+++ frontend/src/app/api/auth.api.js
@@ -1,8 +1,15 @@
 import { http } from "./http";
 
-export async function login({ email, password }){
-    return http("/auth/login", {
-        method: "POST",
-        body: { email, password },
-    });
+export async function login({ email, password }) {
+  return http("/auth/login", {
+    method: "POST",
+    body: { email, password },
+  });
+}
+
+export async function logup({ email, name, lastName, password }) {
+  return http("/auth/logup", {
+    method: "POST",
+    body: { email, name, lastName, password },
+  });
 }
